9bd60c406b3ecca01cf3139a66735edabe5cef84
mostafa sallam <mostaf7583@gmail.com>
2021-11-23 16:52:52 +0200

    basics

---
+++ myapp/app.js
@@ -0,0 +1,24 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+const { title } = require('process');
+
+
+
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+app.get('/',function(req,res)
+{
+  res.render('index',{title: "express"})});
+module.exports = app;
+app,listen(3000);

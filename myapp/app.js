e494702e1e4df37b0c5b82bd9b784a850b33b57c
mostafa sallam <mostaf7583@gmail.com>
2021-11-24 04:59:12 +0200

    get things working

--- myapp/app.js
+++ myapp/app.js
@@ -1,20 +1,26 @@
-var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'ejs');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 
-app.get('/',function(req,res)
-{
-  res.render('index',{title: "express"})});
+app.get('/reg', function (req, res) {
+  res.render('registration', { ppp : "registration" })
+});
+
+app.post('/reg',function(req,res){
+ var x = req.body.username;
+ var y = req.body.password;
+ console.log(x);
+ console.log(y);
+});
 
 module.exports = app;
-app.listen(8080);
+app.listen(3000);

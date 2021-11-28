4bff5683b42a6b3d9408d6a8c49bd9598f4091b6
mostaf7583 <73074293+mostaf7583@users.noreply.github.com>
2021-11-28 15:48:18 +0200

    login reg

--- myapp/app.js
+++ myapp/app.js
@@ -4,6 +4,7 @@ var path = require('path');
 var app = express();
 
 // view engine setup
+
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
@@ -21,52 +22,52 @@ app.get('/', function (req, res) {
 });
 
 // importing home page
-app.get('/home',function(req,res){
+app.get('/home', function (req, res) {
   res.render('home');
 })
 //importing Cart
-app.get('/cart',function(req,res){
+app.get('/cart', function (req, res) {
   res.render('cart');
 });
 // importing sports
-app.get('/sports',function(req,res){
+app.get('/sports', function (req, res) {
   res.render('sports');
 })
 //importing phones
-app.get('/books',function(req,res){
+app.get('/books', function (req, res) {
   res.render('books')
 })
 //importing phones
-app.get('/phones',function(req,res){
+app.get('/phones', function (req, res) {
   res.render('phones')
 })
 //importing sun
-app.get('/sun',function(req,res){
+app.get('/sun', function (req, res) {
   res.render('sun')
 })
 //importing galaxy
-app.get('/galaxy',function(req,res){
+app.get('/galaxy', function (req, res) {
   res.render('galaxy')
 })
 //importing leaves
-app.get('/leaves',function(req,res){
+app.get('/leaves', function (req, res) {
   res.render('leaves')
 })
 //importing searchresults
-app.get('/searchresults',function(req,res){
+app.get('/searchresults', function (req, res) {
   res.render('searchresults')
 })
 //importing tennis
-app.get('/tennis',function(req,res){
+app.get('/tennis', function (req, res) {
   res.render('tennis')
 })
 //importing iphone
 
-app.get('/iphone',function(req,res){
+app.get('/iphone', function (req, res) {
   res.render('iphone')
 })
 //importing boxing
-app.get('/boxing',function(req,res){
+app.get('/boxing', function (req, res) {
   res.render('boxing')
 })
 /////////////////////////////////////////////////////////////////////////##############################//////////////////////////////////////////
@@ -75,47 +76,51 @@ app.get('/boxing',function(req,res){
 // requiring register page usernames and password
 //as i tring to code this function i countered a problem in the ejs file which in the form there is an action i donot know how to handle it 
 
-app.post('/registration',function(req,res){
- var userdata = {username : req.body.username ,password : req.body.password};
- main(userdata)
+app.post('/register', function (req, res) {
+  var userdata = { username: req.body.username, password: req.body.password };
+  console.log(userdata);
+  main(userdata)
 });
 
-app.post('/login',function(req,res){
-  var userdata = {username : req.body.username ,password : req.body.password};
- main1(userdata);
-  
-})
-async function main1(userdata){
-  var { MongoClient, GridFSBucketWriteStream } =require('mongodb');
-  var uri ="mongodb+srv://admin:admin@cluster0.xbuxo.mongodb.net/firstdb?retryWrites=true&w=majority";//our mognodb connection
-  var client =new MongoClient(uri,{useNewUrlParser : true });
+app.post('/login', async function (req, res) {
+  var userdata = { username: req.body.username, password: req.body.password };
+  console.log(userdata);
+  var { MongoClient, GridFSBucketWriteStream } = require('mongodb');
+  var uri = "mongodb+srv://admin:admin@cluster0.xbuxo.mongodb.net/firstdb?retryWrites=true&w=majority";//our mognodb connection
+  var client = new MongoClient(uri, { useNewUrlParser: true });
   await client.connect();
   var output = await client.db('firstdb').collection("second collection").find().toArray();
-  for (let index = 0; index < output.length ; index++) {
-    if (userdata.username == output[index].username && userdata.password==outout[index].username){
-      console.log(true);
+  for (let index = 0; index < output.length; index++) {
+    if (userdata.username === output[index].username && userdata.password === output[index].username) {
+      console.log(userdata);
+      res.redirect('/home');
     }
   }
-    client.close;
-}
-async function main(userdata){
-  var { MongoClient, GridFSBucketWriteStream } =require('mongodb');
-  var uri ="mongodb+srv://admin:admin@cluster0.xbuxo.mongodb.net/firstdb?retryWrites=true&w=majority";//our mognodb connection
-  var client =new MongoClient(uri,{useNewUrlParser : true });
+  client.close;
+
+})
+
+async function main(userdata) {
+  var { MongoClient, GridFSBucketWriteStream } = require('mongodb');
+  var uri = "mongodb+srv://admin:admin@cluster0.xbuxo.mongodb.net/firstdb?retryWrites=true&w=majority";//our mognodb connection
+  var client = new MongoClient(uri, { useNewUrlParser: true });
   await client.connect();
   var output = await client.db('firstdb').collection("second collection").find().toArray();
-  for (let index = 0; index < output.length ; index++) {
-     if (userdata.username == output[index].username){
-       console.log(error);
-     }
-   }
+  var flag = true;
+  for (let index = 0; index < output.length; index++) {
+    if (userdata.username === output[index].username) {
+      console.log("I am here");
+      flag = false;
+    }
+  }
+  if (flag) {
+    await client.db('firstdb').collection("second collection").insertOne(userdata);
+  }
+
   // await client.db('firstdb').Collection("second collection");
   //await client.db('firstdb').collection("second collection").insertOne(user);
-  await client.db('firstdb').collection("second collection").insertOne(userdata);
-  var output = await client.db('firstdb').collection("second collection").find().toArray();
-    client.close;
+  client.close;
 }
 
-main().catch(console.error)
 module.exports = app;
 app.listen(3000);
